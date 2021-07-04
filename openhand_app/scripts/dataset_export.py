fcfd7a3baebc759eade187069a5ccb2c0dc22bb9
ArthurFDLR <arthfind@gmail.com>
2021-07-03 19:32:21 -0500

    🧹Remove flat text format dataset

--- openhand_app/scripts/dataset_export.py
+++ openhand_app/scripts/dataset_export.py
@@ -21,8 +21,35 @@ def loadFile(filePath: Path, shuffle: bool = True):
 
     return np.array(data_out), np.array(accuracy_out)
 
+def generateHandData(labels, dataset_path):
+    data = {"label": [], "hand": [], "accuracy": []}
+    for i in range(21):
+        data.update({"x{}".format(i): [], "y{}".format(i): []})
+    
+    exportNum = 0
+    for label in labels:
+        for hand in [0, 1]:
+            exportNum += 1
+            fileName = label + ["_left", "_right"][hand] + '_hand.json'
+            filePath = dataset_path / fileName
+            
+            if filePath.is_file():
+                list_data, list_accuracy = loadFile(filePath, False)
+                data["label"] += [label] * list_data.shape[0]
+                data["hand"] += ["left" if hand == 0 else "right"] * list_data.shape[0]
+                data["accuracy"] += list(list_accuracy)
+
+                for i in range(21):
+                    data["x{}".format(i)] += list(list_data[:, 0, i])
+                    data["y{}".format(i)] += list(list_data[:, 1, i])
+                
+                print(fileName, 'imported')
+            else:
+                print(fileName, 'not found')
+    return data
+
 if __name__ == "__main__":
-    labels = [
+    handLabels = [
         "0",
         "1",
         "2",
@@ -51,33 +78,11 @@ if __name__ == "__main__":
         "Middle_Finger",
         "Ok",
     ]
-    data = {"label": [], "hand": [], "accuracy": []}
-    for i in range(21):
-        data.update({"x{}".format(i): [], "y{}".format(i): []})
     
     dataset_path = Path(".").resolve() / "Dataset" / "Hands"
     assert dataset_path.is_dir()
-    exportNum = 0
-    for label in labels:
-        for hand in [0, 1]:
-            exportNum += 1
-            fileName = label + ["_left", "_right"][hand] + '_hand.json'
-            filePath = dataset_path / fileName
-            print(fileName)
-            if filePath.is_file():
-                list_data, list_accuracy = loadFile(filePath, False)
-                data["label"] += [label] * list_data.shape[0]
-                data["hand"] += ["left" if hand == 0 else "right"] * list_data.shape[0]
-                data["accuracy"] += list(list_accuracy)
-
-                for i in range(21):
-                    data["x{}".format(i)] += list(list_data[:, 0, i])
-                    data["y{}".format(i)] += list(list_data[:, 1, i])
+    
+    data = generateHandData(handLabels, dataset_path)
 
     df = pd.DataFrame(data)
     df.to_csv(dataset_path.parent / "OpenHand_Dataset.csv", index=False)
-
-    if len(labels)*2 == exportNum:
-        print("All labels exported")
-    else:
-        print(len(labels)*2 - exportNum, "missing files")
